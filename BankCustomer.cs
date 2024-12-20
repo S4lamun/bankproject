cdda626cba04e545538ebd58049bba1192d66641
Jakaz78 <169840759+Jakaz78@users.noreply.github.com>
2024-12-21 00:28:28 +0100

    Adding heritating classes

---
+++ BankCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bankproject
+{
+	public class BankCustomer : Person
+	{
+
+		
+
+		public BankCustomer(string name, string surrname, string pesel, EnumSex sex) : base(name, surrname, pesel, sex) 
+		{
+		}
+	}
+}
---
+++ BankEmployee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//Password nie jest uzywany, probowalem jako dictionary zrobic, ale wtedy id moglo sie powtorzyc z innym haslem.
+namespace bankproject
+{
+	public class BankEmployee : Person
+	{
+
+		private long employeeID;
+		private string employeePassword;
+
+		public BankEmployee(string name, string surrname, string pesel, EnumSex sex, long employeeID, string employeePassword) :base(name, surrname, pesel, sex) 
+		{
+			EmployeeID = employeeID;
+			EmployeePassword = employeePassword;
+
+		}
+
+		public string EmployeePassword			
+		{
+			get => employeePassword;
+			init
+			{
+				if (!Regex.IsMatch(value, @"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$"))
+				{
+					throw new WrongPasswordException("Password must have at least 1 capital letter, 1 digit, and be at least 6 characters long (no special characters).");
+				}
+				employeePassword = value;
+			}
+		}
+
+
+		public long EmployeeID 
+		{
+		get => employeeID;
+		set => employeeID = value;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + $", Employee ID: {employeeID}";
+		}
+
+
+
+
+	}
+}
