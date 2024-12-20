c5b9a1cd0e262ddb6d6c8014a023ecbfa900ece0
S4lamun <143249649+S4lamun@users.noreply.github.com>
2024-12-20 13:26:05 +0100

    Class Person and Class WrongPeselException

---
+++ Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bankproject
+{
+    internal class Person
+    {
+        string name;
+        string surrname;
+        string pesel;
+        #region Properties
+        public string Pesel
+        {
+            get => pesel;
+            set
+            {
+                if(Regex.IsMatch(value,@"\d{11}"))
+                {
+                    throw new WrongPeselException("Pesel is invalid");
+                }
+                pesel = value;
+            }
+        }
+        #endregion
+
+        public Person(string name, string surrname, string pesel)
+        {
+            this.name = name;
+            this.surrname = surrname;
+            Pesel = pesel;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} {surrname}, PESEL: {Pesel}";
+        }
+
+        
+    }
+}
---
+++ WrongPeselException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankproject
+{
+    internal class WrongPeselException:Exception
+    {
+        string message;
+        public WrongPeselException(string message) 
+        {
+            this.message = message;
+        }
+        
+    }
+}
